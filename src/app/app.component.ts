8bcd9113cf1ab584724e068f105520fc40d2a529
angel munoz <angel.munoz@tokiota.com>
2019-06-27 15:52:11 +0200

    create global variable css library and mock for library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'example-lib-app';
+  comic = {
+    id: '1',
+    name: 'DC Comics Rebirth',
+    description: 'It all begins here. Do not skip to the last page. Do not let a friend or message',
+    image: 'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2016/05/DCUREB_Cv1_ds_300dpi_cropped_5743b270aaaae3.39723701.jpg?itok=JpNJJ2_O',
+    price: '$2.99'
+  };
 }
